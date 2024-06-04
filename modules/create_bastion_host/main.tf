e6b8133f601b6bd461751288f8fcdd7f3f4a165d
Jonathan Butler <xxbutler86xx@gmail.com>
2024-06-04 16:59:11 -0400

    modify azure_rm_bastion_host to be standard sku with ip_connect_enabled set to true.

--- modules/create_bastion_host/main.tf
+++ modules/create_bastion_host/main.tf
@@ -12,10 +12,16 @@ resource "azurerm_bastion_host" "bastion" {
   name                = var.bastion_name
   location            = var.location
   resource_group_name = var.resource_group_name
+  sku                 = "Standard"  # Set SKU to Standard
+  ip_connect_enabled = true  # Enable IP-based connectivity
+  file_copy_enabled = true
 
   ip_configuration {
     name                 = "${var.bastion_name}-ipconfig"
     subnet_id            = var.subnet_id
     public_ip_address_id = azurerm_public_ip.bastion_public_ip.id
   }
+
+
 }
+
